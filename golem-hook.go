fbda53edbf425fd435952f8dfe4342a059d77704
Aivan Monceller <aivan@monceller.com>
2018-03-30 12:23:27 +0800

    feat(main): initial golem-hook implementation

--- golem-hook.go
+++ golem-hook.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+const snipLine = "------------------------ >8 ------------------------"
+const commentChar = '#'
 
 func main() {
-	fmt.Println("Hello, Geocine")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "You must provide a path to a file containing"+
+			" the commit message.")
+		os.Exit(1)
+	}
+
+	path := os.Args[1]
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't open file \"%s\".\n", path)
+		os.Exit(1)
+	}
+	msg := string(bytes)
+
+	cleaned := cleanMsg(msg)
+	subject, body := parseMsg(cleaned)
+	fmt.Printf("subject: %s \n", subject)
+	fmt.Printf("body: %s \n", body)
+}
+
+func cleanMsg(msg string) string {
+	remComments := bytes.Buffer{}
+	split := strings.SplitAfter(msg, "\n")
+	for _, line := range split {
+		trim := strings.TrimSpace(line)
+		if strings.HasPrefix(trim, string(commentChar)+" "+snipLine) {
+			break
+		}
+		if strings.HasPrefix(trim, string(commentChar)) {
+			continue
+		}
+
+		remComments.WriteString(line)
+	}
+	return strings.TrimSpace(remComments.String())
+}
+
+func parseMsg(cleanMsg string) (subject string, body string) {
+	split := strings.SplitN(strings.TrimSpace(cleanMsg), "\n\n", 2)
+	subject = split[0]
+	if len(split) > 1 {
+		body = split[1]
+	}
+	return
 }
